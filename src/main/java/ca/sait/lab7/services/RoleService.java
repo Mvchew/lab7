4e37b701d311db4a139a4021a2f4dfbca7dc101c
Melvin Chew <melvincv15@gmail.com>
2022-03-21 23:42:59 -0600

    Create RoleService.java

---
+++ src/main/java/ca/sait/lab7/services/RoleService.java
@@ -0,0 +1,24 @@
+package ca.sait.lab7.services;
+
+
+import ca.sait.lab7.dataaccess.RoleDB;
+import ca.sait.lab7.models.Role;
+import java.util.List;
+
+
+public class RoleService {
+    private RoleDB roleDB = new RoleDB();
+   
+    
+    public List<Role> getAll() throws Exception {
+        
+        List<Role> roles = this.roleDB.getAll();
+        return roles;
+    }
+
+    public int getRoleID(String roleName) throws Exception{
+
+        int id = this.roleDB.lookupRoleID(roleName);
+        return id;
+    }
+}
